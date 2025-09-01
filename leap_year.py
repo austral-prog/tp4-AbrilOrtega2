5599a969f0af5e1c30f68742748ed1c416f91299
AbrilOrtega2 <aortega2@mail.austral.edu.ar>
2025-09-01 12:20:58 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    anio = int(input("Ingrese un año: "))
+    if (anio % 400 == 0) or (anio % 4 == 0 and anio % 100 != 0):
+        print(f"El año {anio} es bisiesto")
+    else:
+        print(f"El año {anio} no es bisiesto")
